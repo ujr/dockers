1cd98e95173f2bbc98b1f73c04bb623d4db54446
Urs-Jakob <741325+ujr@users.noreply.github.com>
2020-11-14 15:37:51 +0100

    duck: make it POSIX compatible

--- duck/log-disk-usage.sh
+++ duck/log-disk-usage.sh
@@ -4,6 +4,7 @@
 # for all directories DIR in BASEDIR $1 by appending a
 # line of the form "YYYY mm dd n MB" to the file $1/DIR.du.
 # ujr/2010-04-05 (based on the 2008-02-05 quick hack)
+# ujr/2020-11-14 (make it POSIX (and BusyBox) compatible)
 
 die() { echo "Fatal: $*"; exit 127; }
 test -z "$1" && die "Usage: $0 <directory>"
@@ -13,12 +14,6 @@ TODAY=`date '+%Y %m %d'` || exit 1
 
 find "$BASEDIR" -mindepth 1 -maxdepth 1 -type d | while read DIR
 do
-  set -- `du -s --block-size=1M "$DIR"` && SIZE=$1
-  echo $TODAY $SIZE MB >> "$DIR.du" # append
+  set -- `du -s -k "$DIR"` && SIZE=$1
+  echo $TODAY $(((SIZE+512)/1024)) MB >> "$DIR.du" # append
 done
-
-#target="$1" || exit 127
-#log="${target%/}.du"
-#today=`date '+%Y %m %d'` || exit 1
-#set -- `du -s "$target"` && blocks=$1 || exit 1
-#echo $today $(((blocks+512)/1024)) >> "$log"
